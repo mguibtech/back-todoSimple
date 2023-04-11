7cf9ccdf433b145cb611f123eb1cbf9c463d5a5c
Marcos <marcos.guibson@precojusto.app.br>
2023-04-11 13:35:57 -0400

    Create UserService

---
+++ src/main/java/com/marcosguibson/todosimple/services/UserService.java
@@ -0,0 +1,57 @@
+package com.marcosguibson.todosimple.services;
+
+import com.marcosguibson.todosimple.models.User;
+import com.marcosguibson.todosimple.repositories.TaskRepository;
+import com.marcosguibson.todosimple.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+//    Autowired e como se fosse o construtor no spring
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private TaskRepository taskRepository;
+
+
+    public User findById(Long id){
+        Optional<User> user = this.userRepository.findById(id);
+//        Se o usuario existir, ele retorna o user, caso contrario ele retornar um exception
+        return user.orElseThrow(()-> new RuntimeException(
+                "Usuario nao encontrado! Id: " + id + ", Tipo: "+ User.class.getName()
+        ));
+    }
+//    Usar sempre que for salvar alguma coisa no banco, para ter um controle melhor (criar, atualizar)
+    @Transactional
+    public User create(User obj){
+        obj.setId(null);
+        obj = this.userRepository.save(obj);
+        this.taskRepository.saveAll(obj.getTasks());
+
+        return obj;
+    }
+
+    @Transactional
+    public User update(User obj){
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+
+        return this.userRepository.save(newObj);
+    }
+
+    public  void delete(Long id){
+        findById(id);
+        try {
+            this.userRepository.deleteById(id);
+        }catch (Exception e){
+            throw  new RuntimeException("Nao e possivel excluir pois ha entidades relacionadas");
+
+        }
+    }
+
+
+}
