5655fac43689674ca90d518247bdcca1ba086409
Marcos <marcos.guibson@precojusto.app.br>
2023-04-12 11:12:44 -0400

    Create webConfig cors

--- src/main/java/com/marcosguibson/todosimple/configs/WebConfig.java
+++ src/main/java/com/marcosguibson/todosimple/configs/WebConfig.java
@@ -1,2 +1,17 @@
-package com.marcosguibson.todosimple.configs;public class WebConfig {
+package com.marcosguibson.todosimple.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.swing.plaf.PanelUI;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**");
+    }
 }
