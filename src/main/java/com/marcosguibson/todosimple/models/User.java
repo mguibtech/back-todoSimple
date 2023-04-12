ccce4938bc8ce89e7b7bc1804c97c0a84d0331c7
Marcos <marcos.guibson@precojusto.app.br>
2023-04-12 11:59:44 -0400

    Add lombok

--- src/main/java/com/marcosguibson/todosimple/models/User.java
+++ src/main/java/com/marcosguibson/todosimple/models/User.java
@@ -16,6 +16,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
 import org.hibernate.annotations.NotFound;
 import org.springframework.lang.NonNull;
 
@@ -24,6 +25,11 @@ import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 @Entity
 @Table(name = User.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 // Anotacoes do spring
 public class User {
 
@@ -55,73 +61,8 @@ public class User {
 
 
   @OneToMany(mappedBy = "user")
+  @JsonProperty(access = Access.WRITE_ONLY)
   private List<Task> tasks = new ArrayList<Task>();
 
-  public List<Task> getTasks() {
-    return this.tasks;
-  }
-  @JsonIgnore
-  public void setTasks(List<Task> tasks) {
-    this.tasks = tasks;
-  }
-
-  public User() {
-  }
-
-  public User(Long id, String username, String password) {
-    this.id = id;
-    this.username = username;
-    this.password = password;
-  }
-
-  public Long getId() {
-    return this.id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getUsername() {
-    return this.username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getPassword() {
-    return this.password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (obj == this)
-      return true;
-    if (obj == null)
-      return false;
-    if (!(obj instanceof User))
-      return false;
-    User other = (User) obj;
-    if (this.id == null)
-      if (other.id != null)
-        return false;
-      else if (!this.id.equals(other.id))
-        return false;
-    return Objects.equals(this.id, other.id) && Objects.equals(this.username, other.username) &&
-        Objects.equals(this.password, other.password);
-  }
-
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
-    return result;
-  }
 
 }
